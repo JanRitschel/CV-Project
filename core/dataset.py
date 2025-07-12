9e5ea69ec66b8ca722ab3fb270e0de59c4fde351
NMichels <nmichels@uni-koblenz.de>
2025-07-12 17:21:33 +0200

    samples.json for time saving in dataset init

--- core/dataset.py
+++ core/dataset.py
@@ -7,7 +7,7 @@ import json
 import tifffile
 
 class PatchDatasetFromJson(Dataset):
-    def __init__(self, root_path, transform=None, channel_indices=[0, 1]):
+    def __init__(self, root_path, transform=None, channel_indices=[0, 1], json_samples_path=None):
         """
         json_path: path to JSON containing structured patch info
         transform: optional image transforms
@@ -34,49 +34,61 @@ class PatchDatasetFromJson(Dataset):
         with open(json_path, 'r') as f:
             data = json.load(f)
 
-        # Iterate through the JSON structure
-        for patient_id, patient_info in data.items():
-            patient_tumor_class = patient_info.get("class", None)
-            slides = patient_info.get("slides", {})
-            
-            # Check if the patient tumor class is valid
-            if patient_tumor_class not in self.label_map:
-                print(f"Warning: Unknown tumor class '{patient_tumor_class}' for {patient_id}")
-                continue
+        # If "Samples" is "None" iterate through the JSON structure to build the 
+        # samples list otherwise use the provided samples
+        if not json_samples_path:
+            for patient_id, patient_info in data.items():
+                patient_tumor_class = patient_info.get("class", None)
+                slides = patient_info.get("slides", {})
+                
+                # Check if the patient tumor class is valid
+                if patient_tumor_class not in self.label_map:
+                    print(f"Warning: Unknown tumor class '{patient_tumor_class}' for {patient_id}")
+                    continue
 
-            # Iterate through slides and their patches
-            for slide_id, slide_info in slides.items():
-                for patch_group, patches in slide_info.items():
-                    if not patch_group.endswith("_patches"):
-                        continue
-                    
-                    if patch_group == "tumor_patches":
-                        # Avoid using the patient tumor class if it is "normal" or None
-                        if patient_tumor_class == "normal" or patient_tumor_class is None:
-                            print(f"Warning: Patient {patient_id} has 'normal' or no class defined but is in 'tumor_patches'. Skipping.")
+                # Iterate through slides and their patches
+                for slide_id, slide_info in slides.items():
+                    for patch_group, patches in slide_info.items():
+                        if not patch_group.endswith("_patches"):
                             continue
-                        # Use the patient tumor class as label
-                        label_str = patient_tumor_class
-                    else:
-                        # Use the patch group name as label since normal and nondiagnostic patches are not tumor patches
-                        label_str = patch_group.replace("_patches", "")
+                        
+                        if patch_group == "tumor_patches":
+                            # Avoid using the patient tumor class if it is "normal" or None
+                            if patient_tumor_class == "normal" or patient_tumor_class is None:
+                                print(f"Warning: Patient {patient_id} has 'normal' or no class defined but is in 'tumor_patches'. Skipping.")
+                                continue
+                            # Use the patient tumor class as label
+                            label_str = patient_tumor_class
+                        else:
+                            # Use the patch group name as label since normal and nondiagnostic patches are not tumor patches
+                            label_str = patch_group.replace("_patches", "")
 
-                    # Get the label index from the label map
-                    label_idx = self.label_map.get(label_str)
-                    if label_idx is None:
-                        print(f"Unknown label '{label_str}', skipping.")
-                        continue
-
-                    # Append each patch with its label in the samples list
-                    for rel_path in patches:
-                        full_path = os.path.join(self.root_dir, rel_path)
-                        # Check if the file exists
-                        if not os.path.exists(full_path):
-                            print(f"Warning: Image file not found: {full_path}")
+                        # Get the label index from the label map
+                        label_idx = self.label_map.get(label_str)
+                        if label_idx is None:
+                            print(f"Unknown label '{label_str}', skipping.")
                             continue
-                        # Append the full path and label index to samples
-                        self.samples.append((full_path, label_idx))
 
+                        # Append each patch with its label in the samples list
+                        for rel_path in patches:
+                            full_path = os.path.join(self.root_dir, rel_path)
+                            # Check if the file exists
+                            if not os.path.exists(full_path):
+                                print(f"Warning: Image file not found: {full_path}")
+                                continue
+                            # Append the full path and label index to samples
+                            self.samples.append((full_path, label_idx))
+            # Save samples as json file
+            with open(os.path.join(self.root_dir, 'meta', 'samples.json'), 'w') as f:
+                json.dump(self.samples, f, indent=4)
+        else:
+            # If json_samples is provided, use it directly
+            if isinstance(json_samples_path, str):
+                with open(json_samples_path, 'r') as f:
+                    self.samples = json.load(f)
+            else:
+                raise ValueError("json_samples must be a path to a JSON file")
+        
         print(f"Loaded {len(self.samples)} patches from JSON.")
 
     def __len__(self):
